36ed2a9f4d6f739a0d24b947789c89bcc4a337ee
Divya <dsrao0712@gmail.com>
2021-04-29 03:07:16 +0530

    Login Signup UI completed

--- flaskapp.py
+++ flaskapp.py
@@ -27,6 +27,13 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
 
 if __name__ == '__main__':
     app.run(debug=True)
