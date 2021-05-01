1425a7e211f0f713813f7d23d893dbbeceac5ba8
Divya <dsrao0712@gmail.com>
2021-05-01 23:17:49 +0530

    Issue fixed , registeration form authentication completed

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from flaskapp import db
+from flaskapp import db, login_manager
+from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
@@ -10,7 +14,6 @@ class User(db.Model):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User, Post
+from flaskapp import app, db, bcrypt
+from flaskapp.forms import RegistrationForm, LoginForm
+from flaskapp.models import User, Post
 
 posts = [
     {
