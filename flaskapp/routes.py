33d74a0a7164bfb456dc9b1253742ca97e38229b
Divya <dsrao0712@gmail.com>
2021-05-09 15:41:59 +0530

    Pagination for Home page added

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template('home.html', posts=posts)
 
 @app.route("/myPosts" ,  methods=['GET'])
@@ -156,3 +157,4 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
