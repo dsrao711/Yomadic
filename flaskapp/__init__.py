ec2ebdc7ce4153f55d7a3e1abc02a691ee8dc827
Divya <dsrao0712@gmail.com>
2021-05-10 18:09:10 +0530

    Password reset completed

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,8 +1,10 @@
-
+import os
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 
 app = Flask(__name__)
@@ -11,5 +13,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER' , 'dsrao0712@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS' , 'iamdivya')
+mail = Mail(app)
 
 from flaskapp import routes
